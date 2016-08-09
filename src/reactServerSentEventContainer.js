f6738a47978a050a833139d9012660f03abd200e
Jeff Salerno <jesalerno84@gmail.com>
2016-08-08 20:45:33 -0400

    moved eventSource declaration to constructor

--- src/reactServerSentEventContainer.js
+++ src/reactServerSentEventContainer.js
@@ -9,18 +9,19 @@ import createEventSource from './createEventSource';
 let nextVersion = 0;
 
 export const serverSentEventConnect = (eventSourceUrl, withCredentials = false, onOpen, onMessage, onError, eventObj) => {
-    const version = nextVersion++
+    const version = nextVersion++;
 
     return ComponentToDecorate => {
         const displayName = `ServerSentEvent${getDisplayName(ComponentToDecorate)}`;
-        const eventSourceProps = new EventSourceProps();
-        const eventSource = createEventSource(eventSourceUrl, withCredentials = false, eventSourceProps, onOpen, onMessage, onError, eventObj)
 
         class ServerSentEventComponent extends Component {
             constructor(props, context) {
                 super(props, context);
                 this.version = version;
                 this.state = {};
+
+                this.eventSourceProps = new EventSourceProps();
+                this.eventSource = createEventSource(eventSourceUrl, withCredentials = false, this.eventSourceProps, onOpen, onMessage, onError, eventObj);
             }
 
             componentDidMount() {
@@ -29,18 +30,19 @@ export const serverSentEventConnect = (eventSourceUrl, withCredentials = false, 
 
             componentWillUnmount() {
                 this._unsubscribe();
+                this.eventSource.close();
             }
 
             render() {
                 const sseProps = this.state.sseProps;
                 return (
-                    <ComponentToDecorate eventSource={eventSource} {...sseProps} />
+                    <ComponentToDecorate eventSource={this.eventSource} {...sseProps} />
                 );
             }
 
             _subscribe() {
                 if (!this.unsubscribe) {
-                    this.unsubscribe = eventSourceProps.subscribe(this._handleChange.bind(this));
+                    this.unsubscribe = this.eventSourceProps.subscribe(this._handleChange.bind(this));
                 }
             }
 
